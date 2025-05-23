6acfb5e74fc0a865b4280c4cd43b02c4370c2c9b
rsmharsha <harsharsm007@gmail.com>
2025-05-23 17:52:07 +0530

    episode 2

--- script.js
+++ script.js
@@ -1,40 +1,12 @@
-/* 
-let root = document.querySelector("#root")
-
-let h1 = document.createElement("h1")
-h1.innerHTML = "Namaste React from JS"
-root.appendChild(h1);
-*/
-
-/*
-
-const heading = React.createElement("h1", {id:"heading", xyz:"abc"}, "Namaste React from React!");
-
-console.log(heading);
-// heading is a React element/object.
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading);
-// render method is responsible for converting heading object into h1 tag and put in in webpage.
-
-*/
-
-/*
-    <div id="parent">
-        <div id="child">
-            <h1>Namaste H1 React!</h1>
-        </div>
-    </div> 
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
 const parent = React.createElement("div", { id: "parent" },
     React.createElement("div", { id: "child1" }, [
-        React.createElement("h1", {}, "Namaste Harsha React!"),
-        React.createElement("h1", {}, "Namaste Rishika React!")
-    ]),
-    React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "Namaste Harsha React!"),
-        React.createElement("h1", {}, "Namaste Rishika React!")
+        React.createElement("h1", {}, "This is WEB DEVELOPMENT!"),
+        React.createElement("h1", {}, "Namaste React! Harsha"),
+        React.createElement("h1", {}, "Namaste React! Rishika")
     ])
 )
 
